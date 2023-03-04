05a434c82b38ae03df7fec05c7064e656640859a
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-03-04 17:59:36 +0100

    Project Files init

--- src/solid/Circle.java
+++ src/solid/Circle.java
@@ -1,4 +1,11 @@
 package solid;
 
 public class Circle {
+
+    private final int radius;
+
+    public Circle(int radius){this.radius = radius;}
+
+    public int getRadius(){return radius;}
+
 }
--- src/solid/Square.java
+++ src/solid/Square.java
@@ -1,4 +1,11 @@
 package solid;
 
 public class Square {
+
+    private final int length;
+
+    public Square(int length){this.length = length;}
+
+    public int getLength(){return length;}
+
 }
