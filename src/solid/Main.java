6e47ed33919f838b749842d076634910538a9087
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-03-05 18:49:36 +0100

    Before Liskov Substitution

--- src/solid/Main.java
+++ src/solid/Main.java
@@ -7,7 +7,11 @@ public class Main {
         AreaCalculator areaCalculator = new AreaCalculator();
         Circle circle = new Circle(10);
         Square square = new Square(10);
-        List<Shape> shapes = List.of(circle, square);
+        NoShape noShape = new NoShape();
+        List<Shape> shapes = List.of(
+                circle,
+                square,
+                noShape);
         int sum = areaCalculator.sum(shapes);
         ShapesPrinter printer = new ShapesPrinter();
         System.out.println(printer.csv(sum));
